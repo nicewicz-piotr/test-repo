9baae90510f69b342f65f7cd34ed629f491552c2
Piotr Nicewicz <nicewicz.piotr@gmail.com>
2020-10-13 20:37:48 +0200

    first repository

---
+++ MyApplication.cs
@@ -0,0 +1,13 @@
+class MyApp
+{
+
+    static void Main()
+    {
+
+        System.Console.WriteLine("Witaj, C#");
+        System.Console.WriteLine("Z drobną modyfikacją");
+        System.Console.WriteLine("Kolejna modyfikacja");
+        System.Console.WriteLine("Jeszcze jedna modyfikacja");
+    }
+
+}
